465e9d62d2f393225a8d369bb09c79ece6d3b521
Laura SAADA <laura.saada2@gmail.com>
2018-02-25 17:21:25 +0100

    modification script run = CubeController

--- TestProject-Unity/Assets/Scripts/CubeController.cs
+++ TestProject-Unity/Assets/Scripts/CubeController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Run : MonoBehaviour {
+public class CubeController : MonoBehaviour {
     public float vitesse = 0.5f; // Déclaration variable pour gérer la vitesse depuis Unity directement grâce au "public"
 
 
@@ -13,10 +13,17 @@ public class Run : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        Deplacement();
+
+
+	}
+
+    void Deplacement()
+    {
         float temps = Time.deltaTime;
         Transform transform = GetComponent<Transform>(); //Déclaration de la variable transform qui est un composant (qui est un objet) Transform. On lui assigne le composant Transform de Cube
         Vector3 deplacement = new Vector3(0, 0, vitesse * temps); // Déclaration nouvelle variable : Création d'un objet Vector3 deplacement qui permettra de modifier la position (en s'ajoutant)
         transform.position = transform.position + deplacement; //On assigne une nouvelle valeur position (dans transform), qui est égale à la position dans transform + la valeur de deplacement
-
-	}
+    }
 }
