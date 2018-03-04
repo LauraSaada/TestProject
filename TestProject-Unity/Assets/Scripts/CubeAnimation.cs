0d8990e7012b127a89d5f1f0934a1ee209e2baf1
TeuTeuf <antoine.teuf@gmail.com>
2018-03-04 16:55:29 +0100

    Add animation script + animation test scene

---
+++ TestProject-Unity/Assets/Scripts/CubeAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAnimation : MonoBehaviour {
+
+    public float animationSpeed = 1f;
+    public Vector3 animationScale = new Vector3(0f, 1f, 0f);
+
+    private Vector3 initScale;
+
+	// Use this for initialization
+	void Start () {
+        initScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = initScale + animationScale * Mathf.Sin(Time.time * animationSpeed);
+	}
+}
